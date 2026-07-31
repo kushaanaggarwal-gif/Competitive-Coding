89d3a700dbd5283b02c7c911fcdc9bd48a0f771b
kushaan2640s <kushaanaggarwal@gmail.com>
2026-07-31 12:52:44 +0530

    experiment

--- Experiment 5/exp_5_STACK-using-QUEUE.py
+++ Experiment 5/exp_5_STACK-using-QUEUE.py
@@ -6,27 +6,55 @@ class Stack:
 
     def push(self, x):
         self.q.append(x)
+        print(x, "pushed into stack")
 
     def pop(self):
         if self.q:
             for i in range(len(self.q) - 1):
                 self.q.append(self.q.popleft())
             return self.q.popleft()
-        return "Stack is empty"
+        return "Stack is Empty"
 
     def top(self):
-        if self.q:      
+        if self.q:
             return self.q[-1]
-        return "Stack is empty"
+        return "Stack is Empty"
+
+    def isEmpty(self):
+        return len(self.q) == 0
 
 
 s = Stack()
 
-s.push(1)
-s.push(2)
-s.push(3)
+while True:
+    print("\nStack Menu")
+    print("1. Push")
+    print("2. Pop")
+    print("3. Top")
+    print("4. Check if Empty")
+    print("5. Exit")
 
-print(s.top())   
-print(s.pop())   
-print(s.pop())   
-print(s.pop())   
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        value = int(input("Enter value to push: "))
+        s.push(value)
+
+    elif choice == 2:
+        print("Removed:", s.pop())
+
+    elif choice == 3:
+        print("Top:", s.top())
+
+    elif choice == 4:
+        if s.isEmpty():
+            print("Stack is Empty")
+        else:
+            print("Stack is Not Empty")
+
+    elif choice == 5:
+        print("Exiting")
+        break
+
+    else:
+        print("Invalid")
